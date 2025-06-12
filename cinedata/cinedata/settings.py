65ea4f5b29ed8416f230f63990d57def73ad2a95
nandakumarp-dev <nandakumarp.dev@gmail.com>
2025-06-12 14:38:09 +0000

    creating models for movie data

--- cinedata/cinedata/settings.py
+++ cinedata/cinedata/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies'
+    'movies','authentication',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -138,5 +139,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'authentication.Profile'
 
 
--- cinedata/movies/models.py
+++ cinedata/movies/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
+import uuid
+
 # Create your models here.
+
+class BaseClass(models.Model):
+
+    uuid = models.SlugField(unique=True,default=uuid.uuid4)
+
+    active_status = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        abstract = True
+
+class IndustryChoices(models.TextChoices):
+
+    BOLLYWOOD = 'Bollywood', 'Bollywood'
+    HOLLYWOOD = 'Hollywood', 'Hollywood'
+    KOLLYWOOD = 'Kollywood', 'Kollywood'
+    TOLLYWOOD = 'Tollywood', 'Tollywood'
+    MOLLYWOOD = 'Mollywood', 'Mollywood'
+    SANDALWOOD = 'Sandalwood', 'Sandalwood'
+    OTHER = 'Other','Other'
+
+class IndustryChoices(models.TextChoices):
+
+    BOLLYWOOD = 'Bollywood', 'Bollywood'
+    HOLLYWOOD = 'Hollywood', 'Hollywood'
+    KOLLYWOOD = 'Kollywood', 'Kollywood'
+    TOLLYWOOD = 'Tollywood', 'Tollywood'
+    MOLLYWOOD = 'Mollywood', 'Mollywood'
+    SANDALWOOD = 'Sandalwood', 'Sandalwood'
+    OTHER = 'Other', 'Other'
+
+class Artist(BaseClass):
+
+    name = models.CharField(max_length=25)
+
+    dob = models.DateField()
+
+    photo = models.ImageField(upload_to='artist/')
+
+    industry = models.CharField(max_length=25)
+
+    proffession = models.CharField(max_length=50)
+
+
+class Movies(BaseClass):
+
+    name = models.CharField(max_length=50)
+
+    released_year = models.CharField(max_length=4)
+
+    # description = models.TextField()
+
+    photo = models.ImageField(upload_to='movies/')
+
+    # cast = models.ManyToManyField()
+
+    # director = models.ForeignKey()
+
+    # production = models.ForeignKey()
+
+    # music_director = models.TextChoices()
+
+
+
+    
